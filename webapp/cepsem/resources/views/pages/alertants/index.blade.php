2a5abb696dc50f8ceef1871c637c1ac4d991c08d
Albert Ricart <albert.ricartt@gmail.com>
2021-03-25 19:59:12 +0100

    imatges

--- webapp/cepsem/resources/views/pages/alertants/index.blade.php
+++ webapp/cepsem/resources/views/pages/alertants/index.blade.php
@@ -1,41 +1,38 @@
 @extends('layouts.layout')
 
 @section('pagename')
-    <span>Alertants</span>
+    Alertants
 @endsection
 
 @section('content')
 
 <div class="container-fluid my-5">
-
-
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th scope="col">Telèfon</th>
-                <th scope="col">Nom</th>
-                <th scope="col">Cognom</th>
-                <th scope="col">Adreça</th>
-                <th scope="col">Municipi</th>
-                <th scope="col">Tipus</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($alertants as $alertant)
-            <tr>
-                <th scope="row">{{ $alertant->telefon }}</th>
-                <td>{{ $alertant->nom }}</td>
-                <td>{{ $alertant->cognoms }}</td>
-                <td>{{ $alertant->adreca }}</td>
-                <td>{{ $alertant->municipi }}</td>
-                <td>{{ $alertant->tipus }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-
-
+    <div class="table-responsive">
+        <table class="table table-hover">
+            <thead>
+                <tr class="table-danger">
+                    <th scope="col">Telèfon</th>
+                    <th scope="col">Nom</th>
+                    <th scope="col">Cognom</th>
+                    <th scope="col">Adreça</th>
+                    <th scope="col">Municipi</th>
+                    <th scope="col">Tipus</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($alertants as $alertant)
+                <tr class="table-info py-5" >
+                    <th scope="row">{{ $alertant->telefon }}</th>
+                    <td>{{ $alertant->nom }}</td>
+                    <td>{{ $alertant->cognoms }}</td>
+                    <td>{{ $alertant->adreca }}</td>
+                    <td>{{ $alertant->municipi }}</td>
+                    <td>{{ $alertant->tipus }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
 </div>
 
