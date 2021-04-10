2685d85d93fb18adc7e81b3b46121712a4c26ac9
Ivan Cabañas <ivancg86@gmail.com>
2021-04-10 12:38:41 +0200

    update tipus

--- webapp/cepsem/resources/views/pages/cecos/altres/tipusAlertant/index.blade.php
+++ webapp/cepsem/resources/views/pages/cecos/altres/tipusAlertant/index.blade.php
@@ -63,36 +63,68 @@ Tipus Alertants
                           </div>
 
 
-                          {{-- MODAL CREAR/UPDATE --}}
+                          {{-- MODAL CREAR --}}
                           <div class="modal" id="modalCrear" tabindex="-1" role="dialog">
-                                <form action="{{ action([App\Http\Controllers\TipusAlertantController::class, 'store']) }}" method="POST">
-                                @csrf
-                            <div class="modal-dialog" role="document">
-                              <div class="modal-content">
-                                <div class="modal-header">
+                              <form action="{{ action([App\Http\Controllers\TipusAlertantController::class, 'store']) }}" method="POST">
+                              @csrf
+                          <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                              <div class="modal-header">
 
-                                  <h5 class="modal-title">Alertant</h5>
-                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                  </button>
-                                </div>
-                                <div class="modal-body">
-                                    <div class="form-group row">
-                                        <label class="col-2" for="sigles">Tipus</label>
-                                        <div class="col-10">
-                                            <input class="form-control" type="text" name="tipus" id="tipus" value="{{ old('tipus') }}">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fas fa-times"></i>Tancar</button>
-                                    <button type="submit" class="btn btn-danger float-right" ><i class="fas fa-trash-alt"></i>Aceptar</button>
-                                </div>
+                                <h5 class="modal-title">Alertant</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                  <span aria-hidden="true">&times;</span>
+                                </button>
+                              </div>
+                              <div class="modal-body">
+                                  <div class="form-group row">
+                                      <label class="col-2" for="sigles">Tipus</label>
+                                      <div class="col-10">
+                                          <input class="form-control" type="text" name="tipus" id="tipus">
+                                      </div>
+                                  </div>
+                              </div>
+                              <div class="modal-footer">
+                                  <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fas fa-times"></i>Tancar</button>
+                                  <button type="submit" class="btn btn-danger float-right" ><i class="fas fa-trash-alt"></i>Aceptar</button>
                               </div>
                             </div>
-                        </form>
                           </div>
-                    </td>
+                      </form>
+                        </div>
+
+
+                          {{-- MODAL UPDATE --}}
+                          <div class="modal" id="modalCrear" tabindex="-1" role="dialog">
+                                <form action="{{ action([App\Http\Controllers\TipusAlertantController::class, 'update'], ['tipus_alertant' => $alertant->id]) }}" method="POST">
+                                    @csrf
+                                    @method('PUT')
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+
+                                                <h5 class="modal-title">Alertant</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div class="form-group row">
+                                                    <label class="col-2" for="sigles">Tipus</label>
+                                                    <div class="col-10">
+                                                        <input class="form-control" type="text" name="tipus" id="tipus" value="{{ old('tipus') }}">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fas fa-times"></i>Tancar</button>
+                                                <button type="submit" class="btn btn-danger float-right" ><i class="fas fa-trash-alt"></i>Aceptar</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </td>
                 </tr>
             @endforeach
         </tbody>
