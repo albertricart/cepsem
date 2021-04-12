5ac6a5010d639266803fa999a2a5850a568c7bfa
Albert Ricart <albert.ricartt@gmail.com>
2021-04-12 20:37:30 +0200

    cecos cards

--- webapp/cepsem/resources/views/pages/cecos/index.blade.php
+++ webapp/cepsem/resources/views/pages/cecos/index.blade.php
@@ -14,34 +14,35 @@ CECOS
 @endsection
 
 @section('content')
-<div class="container-xl">
-    <div class="row mt-5">
-        <div class="col-md-6">
-            <menucard-component size="lg" page="incidencies" name="INCIDÈNCIES"></menucard-component>
-        </div>
+<div class="cecos">
+    <div class="container-xl">
+        <div class="row mt-5">
+            <div class="col-md-6">
+                <menucard-component size="lg" page="incidencies" name="INCIDÈNCIES"></menucard-component>
+            </div>
 
-        <div class="col-md-6">
-            <div class="row">
-                <div class="col-md-12">
-                    <menucard-component size="sm" page="recursos-mobils" name="RECURSOS MÒBILS"></menucard-component>
-                </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-12">
+                        <menucard-component size="sm" page="recursos-mobils" name="RECURSOS MÒBILS"></menucard-component>
+                    </div>
 
-                <div class="col-md-12">
-                    <menucard-component size="sm" page="alertants" name="ALERTANTS"></menucard-component>
+                    <div class="col-md-12">
+                        <menucard-component size="sm" page="alertants" name="ALERTANTS"></menucard-component>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-md-6">
-            <menucard-component size="sm" page="usuaris" name="USUARIS"></menucard-component>
-        </div>
+            <div class="col-md-6">
+                <menucard-component size="sm" page="usuaris" name="USUARIS"></menucard-component>
+            </div>
 
-        <div class="col-md-6">
-            <menucard-component size="sm" page="altres" name="ALTRES"></menucard-component>
-        </div>
+            <div class="col-md-6">
+                <menucard-component size="sm" page="altres" name="ALTRES"></menucard-component>
+            </div>
 
+        </div>
     </div>
 </div>
-
 
 @endsection
